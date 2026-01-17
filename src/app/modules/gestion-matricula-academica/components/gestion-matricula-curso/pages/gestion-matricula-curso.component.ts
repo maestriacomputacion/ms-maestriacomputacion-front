2e4c9346348203af39975a6f7f0ff5390b7f9137
Javier Rojas <javierra@unicauca.edu.co>
2026-01-16 22:23:48 -0500

    feat: agregar comentario en el método ngOnInit para mayor claridad

--- src/app/modules/gestion-matricula-academica/components/gestion-matricula-curso/pages/gestion-matricula-curso.component.ts
+++ src/app/modules/gestion-matricula-academica/components/gestion-matricula-curso/pages/gestion-matricula-curso.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class GestionMatriculaCursoComponent implements OnInit {
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        // Método inicializado
+    }
 }
